f2c1ad0e1cf3c1778330cf21e631da026bea67f6
regaaron <aaron_lopez222@hotmail.com>
2024-06-08 10:11:11 -0600

    Cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componentes/header/header.component';
 import { Api1Component } from './componentes/api1/api1.component';
 import { Api2Component } from './componentes/api2/api2.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,Api1Component,Api2Component],
+  imports: [RouterOutlet,HeaderComponent,Api1Component,Api2Component,InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
