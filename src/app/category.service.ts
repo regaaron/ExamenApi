5a8a9fb705242acb42547125def55673271921ff
regaaron <aaron_lopez222@hotmail.com>
2024-06-08 12:46:43 -0600

    Api2 lista

---
+++ src/app/category.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:3000/api/category';
+  private mealsByCategoryUrl = 'http://localhost:3000/api/meals/';
+
+
+  constructor(private http: HttpClient) { }
+
+  getCategory(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  getMealsByCategory(category: string): Observable<any> {
+    return this.http.get<any>(`${this.mealsByCategoryUrl}${category}`);
+  }
+  }
--- src/app/componentes/api2/api2.component.ts
+++ src/app/componentes/api2/api2.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../category.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-api2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './api2.component.html',
   styleUrl: './api2.component.css'
 })
 export class Api2Component {
+  categories: any[] = [];
+  selectedCategory: string = '';
+  meals: any[] = [];
+  constructor(private categoryService: CategoryService){ }
+
+  ngOnInit(): void{
+    this.categoryService.getCategory().subscribe(data =>{
+      this.categories = data.categories;
+    });
+  }
+
+  // onCategoryChange(category: string):void{
+  //   this.selectedCategory = category;
+  //   this.categoryService.getMealsByCategory(category).subscribe(data =>{
+  //     this.meals = data.meals;
+  //   });
+
+  // }
+
+
+
+  onCategoryChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedCategory = selectedValue; // Assign selectedValue instead of event
+    this.categoryService.getMealsByCategory(selectedValue).subscribe(data => {
+      this.meals = data.meals;
+    });
+  }
+  
+ 
+  
 
 }
