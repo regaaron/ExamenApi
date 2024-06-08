d238421cb1febf4b96bb2130dba55e483518196e
regaaron <aaron_lopez222@hotmail.com>
2024-06-08 11:49:51 -0600

    Api1 lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { Api1Component } from './componentes/api1/api1.component';
 import { Api2Component } from './componentes/api2/api2.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,Api1Component,Api2Component,InicioComponent],
+  imports: [RouterOutlet,HeaderComponent,Api1Component,Api2Component,InicioComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/api1/api1.component.ts
+++ src/app/componentes/api1/api1.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { MealService } from '../../meal.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-api1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './api1.component.html',
   styleUrl: './api1.component.css'
 })
 export class Api1Component {
+  mealName: string = '';
+  mealData: any = null;
 
+  constructor(private mealService: MealService) { }
+
+  searchMeal() {
+    this.mealService.getMealByName(this.mealName).subscribe(data => {
+      this.mealData = data.meals ? data.meals[0] : null;
+    });
+  }
+
+  getIngredients(): string[] {
+    const ingredients: string[] = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = this.mealData[`strIngredient${i}`];
+      const measure = this.mealData[`strMeasure${i}`];
+      if (ingredient) {
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    return ingredients;
+  }
 }
